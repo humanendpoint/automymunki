868b002829ecb8e4f21d7691ef9644258c70fcd3
Tom T <tunberg@protonmail.ch>
2024-10-24 22:54:51 +0200

    Update generate_wiki.py

--- autopkg/helpers/generate_wiki.py
+++ autopkg/helpers/generate_wiki.py
@@ -197,7 +197,7 @@ def gather_munki_info(workspace_directory, folder_path):
 
 
 def get_latest_tag():
-    repo_url = f"https://api.github.com/repos/humanendpoint/automaticmunki/tags"
+    repo_url = f"https://api.github.com/repos/{repo_name}/tags"
     github_headers = {
         "Content-Type": "application/json",
         "Authorization": f"token {os.environ.get('GITHUB_TOKEN')}",
